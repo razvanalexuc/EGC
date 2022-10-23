2b181823f5b86cc26fb715993f2071c116d98647
razvanalexuc <101744477+razvanalexuc@users.noreply.github.com>
2022-10-23 22:15:56 +0300

    Laborator EGC #02

---
+++ Lab2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+
+/**
+    Aplicația utilizează biblioteca OpenTK v2.0.0 (stable) oficială și OpenTK. GLControl v2.0.0
+    (unstable) neoficială. Aplicația fiind scrisă în modul consolă nu va utiliza controlul WinForms
+    oferit de OpenTK!
+    Tipul de ferestră utilizat: GAMEWINDOW. Se demmonstrează modul imediat de randare (vezi comentariu!)...
+**/
+namespace Lab2
+{
+    class SimpleWindow : GameWindow
+    {
+
+        // Constructor.
+        public SimpleWindow() : base(1024, 768)
+        {
+            KeyDown += Keyboard_KeyDown;
+        }
+
+        // Tratează evenimentul generat de apăsarea unei taste. Mecanismul standard oferit de .NET
+        // este cel utilizat.
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+        }
+
+        // Setare mediu OpenGL și încarcarea resurselor (dacă e necesar) - de exemplu culoarea de
+        // fundal a ferestrei 3D.
+        // Atenție! Acest cod se execută înainte de desenarea efectivă a scenei 3D.
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+        // Inițierea afișării și setarea viewport-ului grafic. Metoda este invocată la redimensionarea
+        // ferestrei. Va fi invocată o dată și imediat după metoda ONLOAD!
+        // Viewport-ul va fi dimensionat conform mărimii ferestrei active (cele 2 obiecte pot avea și mărimi 
+        // diferite). 
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        // Secțiunea pentru "game logic"/"business logic". Tot ce se execută în această secțiune va fi randat
+        // automat pe ecran în pasul următor - control utilizator, actualizarea poziției obiectelor, etc.
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Momentan aplicația nu face nimic!
+        }
+
+        // Secțiunea pentru randarea scenei 3D. Controlată de modulul logic din metoda ONUPDATEFRAME.
+        // Parametrul de intrare "e" conține informatii de timing pentru randare.
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // Modul imediat! Suportat până la OpenGL 3.5 (este ineficient din cauza multiplelor apeluri de
+            // funcții).
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+            // Sfârșitul modului imediat!
+
+            this.SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            // Utilizarea cuvântului-cheie "using" va permite dealocarea memoriei o dată ce obiectul nu mai este
+            // în uz (vezi metoda "Dispose()").
+            // Metoda "Run()" specifică cerința noastră de a avea 30 de evenimente de tip UpdateFrame per secundă
+            // și un număr nelimitat de evenimente de tip randare 3D per secundă (maximul suportat de subsistemul
+            // grafic). Asta nu înseamnă că vor primi garantat respectivele valori!!!
+            // Ideal ar fi ca după fiecare UpdateFrame să avem si un RenderFrame astfel încât toate obiectele generate
+            // în scena 3D să fie actualizate fără pierderi (desincronizări între logica aplicației și imaginea randată
+            // în final pe ecran).
+            using (SimpleWindow example = new SimpleWindow())
+            {
+
+                // Verificați semnătura funcției în documentația inline oferită de IntelliSense!
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
